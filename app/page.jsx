edfdaa6902b095d5e7c70a8bc6eb1a8ecb4fa80e
Jissmon Jose <jissmon476@gmail.com>
2024-07-13 10:28:00 +0530

    commitall changes new

--- app/page.jsx
+++ app/page.jsx
@@ -2,12 +2,14 @@ import Link from 'next/link'
 import React from 'react'
 import Hero from '../components/Hero'
 import InfoBoxes from '@/components/InfoBoxes'
+import HomeProperties from '@/components/HomeProperties'
 
 const HomePage = () => {
   return (
     <div>
      <Hero />
      <InfoBoxes />
+     <HomeProperties />
     </div>
   )
 }
