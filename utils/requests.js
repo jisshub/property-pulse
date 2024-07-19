2f8d4d410239ed4f526dc93f01422394939b3155
Jissmon Jose <jissmon476@gmail.com>
2024-07-19 18:57:56 +0530

    commit all changes

--- utils/requests.js
+++ utils/requests.js
@@ -1,4 +1,6 @@
 const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN || null
+
+// Fetch all properties
 async function fetchProperties() {
     try {
 
@@ -6,7 +8,7 @@ async function fetchProperties() {
       if (!apiDomain) {
         return []
       }
-      
+
       const res = await fetch(`${apiDomain}/properties`)
   
       if (!res.ok) {
@@ -20,4 +22,26 @@ async function fetchProperties() {
     }
   }
 
-export default fetchProperties
+
+// Fetch single property
+async function fetchProperty(id) {
+  try {
+    // Handle the case where the domain is not available yet
+    if (!apiDomain) {
+      return null;
+    }
+
+    const res = await fetch(`${apiDomain}/properties/${id}`);
+
+    if (!res.ok) {
+      throw new Error('Failed to fetch data');
+    }
+
+    return res.json();
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+}
+
+export { fetchProperties, fetchProperty };
