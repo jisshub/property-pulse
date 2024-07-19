e6f2154f267affd93366521291abc4a62ee0c661
Jissmon Jose <jissmon476@gmail.com>
2024-07-19 14:32:11 +0530

    commit all changes

--- app/api/properties/route.jsx
+++ app/api/properties/route.jsx
@@ -1,12 +1,13 @@
 import connectDB from "@/config/database"
+import Property from "@/models/Property"
 
 export const GET = async (request) => {
     try {
         // connect to DB
         await connectDB()
-        return new Response(JSON.stringify({
-            message: 'Properties fetched successfully',
-        }), { status: 200 })
+        const properties =await Property.find();
+
+        return new Response(JSON.stringify(properties), { status: 200 })
     } catch (error) {
         console.log(error)
         return new Response(error.message, { status: 500 })
--- app/page.jsx
+++ app/page.jsx
@@ -3,8 +3,10 @@ import React from 'react'
 import Hero from '../components/Hero'
 import InfoBoxes from '@/components/InfoBoxes'
 import HomeProperties from '@/components/HomeProperties'
+import { connectDB } from '@/config/database'
 
 const HomePage = async () => {
+  await connectDB();
   return (
     <div>
      <Hero />
