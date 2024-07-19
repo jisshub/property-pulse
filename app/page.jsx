9ee23a0a4dab93622e40b68293c662f1f3bd8d79
Jissmon Jose <jissmon476@gmail.com>
2024-07-19 14:52:33 +0530

    commit all changes

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import Hero from '../components/Hero'
 import InfoBoxes from '@/components/InfoBoxes'
 import HomeProperties from '@/components/HomeProperties'
-import { connectDB } from '@/config/database'
+import connectDB from '../config/database'
 
 const HomePage = async () => {
   await connectDB();
--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 import properties from '../../properties.json';
 import PropertyCard from '@/components/PropertyCard';
 
-const PropertiesPage = () => {
+async function fetchProperties() {
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_DOMAIN}/properties`)
+
+    if (!res.ok) {
+      throw new Error('Failed to fetch properties')
+    } else {
+      return res.json()
+    }
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const PropertiesPage = async () => {
+  const properties = await fetchProperties()
   console.log('properties', properties);
   return (
     <section className="px-4 py-6">
